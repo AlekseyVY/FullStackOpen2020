2c674c0d3f9b360aeb1e47078e6594d43f097a99
AlekseyVY <alekseyVY@gmail.com>
2020-05-12 22:32:10 +0400

    put, update on server side

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,27 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-    return (
-        <>
-            <p>Hello {props.name}, you are {props.age} years old</p>
-        </>
-    )
-}
-
-
-
-const App = () => {
-
-    return (
-    <>
-        <h1>Greetings</h1>
-        <Hello name={'Aleksey'} age={38}/>
-        <Hello name={'Kirill'} age={8}/>
-        <Hello name={'Otez'} age={60}/>
-    </>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
