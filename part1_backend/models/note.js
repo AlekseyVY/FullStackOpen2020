2a3f04346d6b5f59318c6c47d63273ee3eb9336e
AlekseyVY <alekseyVY@gmail.com>
2020-05-19 21:03:14 +0400

    learning 4b

--- part1_backend/models/note.js
+++ part1_backend/models/note.js
@@ -1,24 +1,12 @@
-require('dotenv').config({path: '.env'})
 const mongoose = require('mongoose')
+
 mongoose.set('useFindAndModify', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
 
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
-        minLength: 5,
-        required: true
+        required: true,
+        minLength: 5
     },
     date: {
         type: Date,
@@ -34,5 +22,6 @@ noteSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
+
 
 module.exports = mongoose.model('Note', noteSchema)
