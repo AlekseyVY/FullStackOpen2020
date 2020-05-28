75dcd80710c9f17241617caf6bd9abbf33db860e
AlekseyVY <alekseyVY@gmail.com>
2020-05-28 20:49:44 +0400

    exercises 6.5*

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -18,10 +18,20 @@ const App = () => {
       dispatch(addNewAnecdote(content))
   }
 
+    const compare = (a, b) => {
+        if(a.votes < b.votes){
+            return 1
+        } else if(a.votes > b.votes){
+            return -1
+        } else {
+            return 0
+        }
+    }
+
   return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.map(anecdote =>
+        {anecdotes.sort(compare).map(anecdote =>
             <div key={anecdote.id}>
               <div>
                 {anecdote.content}
