26814f074e596902f66fd51ed2adaede6a6b4d1a
AlekseyVY <alekseyVY@gmail.com>
2020-05-30 08:44:57 +0400

    learning 6c_part1

--- application/src/index.js
+++ application/src/index.js
@@ -5,8 +5,6 @@ import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
 import App from './App'
 import { Provider } from 'react-redux'
-import { createNote } from "./reducers/noteReducer";
-import { filterChange } from "./reducers/filterReducer";
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
@@ -15,10 +13,7 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer, composeWithDevTools())
-console.log(store.getState())
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
 
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
--- application/src/reducers/noteReducer.js
+++ application/src/reducers/noteReducer.js
@@ -1,20 +1,6 @@
-const initialState = [
-    {
-        content: 'reducer defines how redux store works',
-        important: true,
-        id: 1
-    },
-    {
-        content: 'state of store can contain any data',
-        important: false,
-        id: 2
-    }
-]
 
 
-
-
-const noteReducer = (state = initialState, action) => {
+const noteReducer = (state = [], action) => {
     switch(action.type){
         case 'NEW_NOTE':
             return state.concat(action.data)
@@ -27,21 +13,25 @@ const noteReducer = (state = initialState, action) => {
             }
             return state.map(note => note.id !== id ? note : changedNote)
         }
+        case 'INIT_NOTES':
+            return action.data
         default:
             return state
     }
 }
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+export const initializeNotes = (notes) => {
+    return {
+        type: 'INIT_NOTES',
+        data: notes
+    }
+}
 
-export const createNote = (content) => {
+
+export const createNote = (data) => {
     return {
         type: 'NEW_NOTE',
-        data: {
-            content,
-            important: false,
-            id: generateId()
-        }
+        data
     }
 }
 
